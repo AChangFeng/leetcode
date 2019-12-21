c9a42db5b602a9cb660f5363f820e3a68861320c
Huqin <huqin@gan-dao.com>
2019-12-21 17:32:06 +0800

    287. Find the Duplicate Number

---
+++ algorithms/findDuplicate.py
@@ -0,0 +1,59 @@
+#! python3
+# -*- coding:UTF-8 -*-
+"""
+#287. Find the Duplicate Number
+# Created on 2019/12/21 16:49
+# findDuplicate
+# @author: ChangFeng
+"""
+from typing import List
+
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        """
+        sort
+        """
+        nums = sorted(nums)
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                return nums[i]
+
+    def findDuplicate(self, nums: List[int]) -> int:
+        """
+        set
+        """
+        s = set()
+        for n in nums:
+            if n in s:
+                return n
+            else:
+                s.add(n)
+
+    def findDuplicate(self, nums: List[int]) -> int:
+        t = r = nums[0]
+        while True:
+            t = nums[t]
+            r = nums[nums[r]]
+            if t == r:
+                break
+        p1, p2 = nums[0], t
+        while p1 != p2:
+            p1 = nums[p1]
+            p2 = nums[p2]
+        return p1
+
+
+def test(test_case, expect):
+    r = Solution().findDuplicate(test_case)
+    assert r == expect, "test_case fail. input: {}, expect: {}, actual: {}".format(test_case, expect, r)
+
+
+def main():
+    test_cases = [([1, 3, 4, 2, 2], 2), ([3, 1, 3, 4, 2], 3)]
+    for test_case in test_cases:
+        test(*test_case)
+
+
+if __name__ == '__main__':
+    main()
