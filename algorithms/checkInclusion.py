286b8e5a35e459d5bfa78f60b5b33a862886efb9
huqin <huqin@gan-dao.com>
2020-04-22 10:01:45 +0800

    567. Permutation in String

--- algorithms/checkInclusion.py
+++ algorithms/checkInclusion.py
@@ -1,11 +1,13 @@
 #! python3
 # -*- coding:UTF-8 -*-
 """
+# 567. Permutation in String
 # Created on #DATE #TIME
 # #NAME
 # @author: ChangFeng
 """
 import datetime
+from collections import defaultdict
 
 
 class Solution:
@@ -36,13 +38,43 @@ class Solution:
                 s2_dict[s2[right]] = s2_dict.get(s2[right], 0) + 1
         return False
 
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        need = defaultdict(int)
+        for c in s1:
+            need[c] += 1
+        window = defaultdict(int)
+        left = right = valid = 0
+        while right < len(s2):
+            c = s2[right]
+            # 扩展窗口
+            right += 1
+            # 更新窗口数据
+            if c in need:
+                window[c] += 1
+                if window[c] == need[c]:
+                    valid += 1
+            if valid == len(need):
+                return True
+            # 是否收缩窗口
+            while right - left >= len(s1):
+                # 更新窗口数据
+                d = s2[left]
+                # 收缩窗口
+                left += 1
+                if d in need:
+                    if window[d] == need[d]:
+                        valid -= 1
+                    window[d] -= 1
+        return False
+
 
 def main():
     _input1 = 'ab'
     _input2 = 'eidbaoo'
     now = datetime.datetime.now()
     print(Solution().checkInclusion(_input1, _input2))
-    print(datetime.datetime.now() - now)
 
 
 if __name__ == '__main__':
